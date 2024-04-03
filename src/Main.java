484d87ddb1264c516a583aa86b729cb370e64237
Maria Kuleva <mariaqleva@gmail.com>
2024-04-03 22:20:49 +0300

    programm

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,31 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int account = 500;
+        int payment = 1200;
+        int bonus = (payment / 100);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        if (payment >= 1000) {
+            int total = (account + payment + bonus);
+            System.out.println("На вашем счету: " + total + " руб.");
+            System.out.println("Начислено бонусов: " + bonus + " руб.");
+        } else {
+            int total = (account + payment);
+            System.out.println("На вашем счету: " + total + " руб.");
         }
+
+
+        //System.out.println("На вашем счету: " + total + " руб.");
     }
+    //if (payment >=1000) {
+    //int total = (total + bonus);
 }
+
+        //if (payment >= 1000) {
+
+
+
+
+
+
